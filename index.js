d84d236bf21721f67f8edf0a8f8333f55b9e494a
auma2022 <auma.amwayi@student.moringaschool.com>
2022-06-02 02:36:28 +0300

    commit tree 69398fd5d1b40b500c36cf29a352df39c4d9b2eb

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (Jardson) {
+  cats.push(Jardson);
+}
+function destructivelyPrependCat (Jardson) {
+  cats.unshift(Jardson);
+}
+function destructivelyRemoveLastCat () {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+  cats.shift();
+}
+function appendCat (name) {
+  return [...cats, name];
+}
+
+function prependCat (name) {
+  return [name, ...cats];
+}
